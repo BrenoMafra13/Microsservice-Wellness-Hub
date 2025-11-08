7cdb201f9c00e5e7bd199d1819ef9b5291e69e3b
BrenoMafra13 <brenolopesmafra@gmail.com>
2025-11-07 21:46:27 -0500

    Fixing structure and finishing project

--- microsservices-parent/event-service/src/main/java/ca/gbc/comp3095/eventservice/dto/EventResponse.java
+++ microsservices-parent/event-service/src/main/java/ca/gbc/comp3095/eventservice/dto/EventResponse.java
@@ -3,7 +3,7 @@ package ca.gbc.comp3095.eventservice.dto;
 import java.time.LocalDate;
 
 public record EventResponse(
-        Long eventId,
+        Long id,
         String title,
         String description,
         LocalDate date,
--- microsservices-parent/event-service/src/main/java/ca/gbc/comp3095/eventservice/model/Event.java
+++ microsservices-parent/event-service/src/main/java/ca/gbc/comp3095/eventservice/model/Event.java
@@ -16,6 +16,7 @@ import java.time.LocalDate;
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
     private Long id;
 
     private String title;
